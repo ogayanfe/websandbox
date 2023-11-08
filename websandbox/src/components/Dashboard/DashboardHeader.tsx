996df8bea86e9ad6e291f1cb1e8f71a2e3b80351
Odule Ayanfeoluwa Grandeur <95624629+ogayanfe@users.noreply.github.com>
2023-11-08 01:25:53 +0000

    Added github link to dashboard header

--- websandbox/src/components/Dashboard/DashboardHeader.tsx
+++ websandbox/src/components/Dashboard/DashboardHeader.tsx
@@ -2,10 +2,15 @@ import { IconButton, Button, Avatar } from "@mui/material";
 import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
 import AddRoundedIcon from "@mui/icons-material/AddRounded";
 import useAuthContext from "../../contexts/authContext";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { useEffect } from "react";
 
 export default function DashboardHeader() {
   const textColor = " rgb(134 134 134);";
   const authContext = useAuthContext();
+  useEffect(() => {
+    document.title = "WebSandbox | Dashboard"
+  }, [])
   return (
     <div className="W-full p-2 px-4 text-lg">
       <nav className="flex w-fill items-center justify-between">
@@ -27,6 +32,10 @@ export default function DashboardHeader() {
           <IconButton aria-label="Notification">
             <NotificationsRoundedIcon className="bg-inherit" sx={{ color: textColor }} />
           </IconButton>
+          <a href="https://github.com/ogayanfe/websandbox" target="_blank" className="flex items-center">
+            <span className="fixed left-[-1000000000000px]">Github Link</span>
+            <GitHubIcon sx={{color: textColor}}/>
+          </a>
           <Avatar sx={{ width: 30, height: 30 }} variant="circular">
             {authContext?.user?.username[0].toUpperCase()}
           </Avatar>
