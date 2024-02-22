0b720d2b95e46add7b769082ac0b4bdb7ea9a7cd
O.G Ayanfe <ayanfeoluwakoyinsola@gmail.com>
2024-02-22 15:43:11 +0100

    Update dashboard ui

--- websandbox/src/components/Dashboard/index.tsx
+++ websandbox/src/components/Dashboard/index.tsx
@@ -29,22 +29,41 @@ export default function DashboardBase() {
   });
 
   return (
-    <div className="flex flex-col items-center">
-      <h2 className="text-xl xl:text-2xl font-semibold text-center text-gray-800 dark:text-blue-100 p-4 pb-8">
-        Welcome back, <span className="italic capitalize">{authContext?.user?.username}</span>
-      </h2>
-      <Tabs
-        variant="scrollable"
-        component="nav"
-        aria-label="select tab to view"
-        value={tabValue}
-        onChange={(_, v) => setTabValue(v)}
-      >
-        <Tab label="Your Work" component={Link} value="work" to="./" />
-        <Tab label="Starred projects" component={Link} value="starred" to="./starred" />
-        <Tab label="Update Profile" component={Link} value="update" to="./update" />
-      </Tabs>
-      <Outlet />
+    <div className="flex flex-col items-center h-full">
+      <div className="w-full flex h-full flex-grow max-lg:flex-col max-lg:items-center justify-center">
+        <div className="w-[300px] max-lg:hidden pt-16 ">
+          <Tabs
+            variant="scrollable"
+            component="nav"
+            orientation="vertical"
+            aria-label="select tab to view"
+            value={tabValue}
+            onChange={(_, v) => setTabValue(v)}
+          >
+            <Tab label="Your Work" component={Link} value="work" to="./" />
+            <Tab label="Starred projects" component={Link} value="starred" to="./starred" />
+          </Tabs>
+        </div>
+        <div className="flex-grow w-full h-full flex flex-col items-center justify-center">
+          <h2 className="text-xl xl:text-2xl font-semibold text-center text-gray-800 dark:text-blue-100 p-4 pb-8">
+            Welcome back, <span className="italic capitalize">{authContext?.user?.username}</span>
+          </h2>
+          <div className="lg:hidden">
+            <Tabs
+              variant="scrollable"
+              component="nav"
+              // orientation="vertical"
+              aria-label="select tab to view"
+              value={tabValue}
+              onChange={(_, v) => setTabValue(v)}
+            >
+              <Tab label="Your Work" component={Link} value="work" to="./" />
+              <Tab label="Starred projects" component={Link} value="starred" to="./starred" />
+            </Tabs>
+          </div>
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
